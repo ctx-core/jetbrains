fd556c020dae57d1dedcecb8f7d15d060f22c8f3
Brian Takita <brian.takita@gmail.com>
2020-09-13 22:28:14 -0700

    ↑ 2.0.0
    - idea-exclude-config instead of idea-exclude-config.js

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# @ctx-core/jetbrains
+
+## 2.0.0
+### Major Changes
+
+- idea-exclude-config instead of idea-exclude-config.js
--- bin/idea-exclude-config.js
+++ bin/idea-exclude-config.js
@@ -6,21 +6,22 @@ const fs = require('fs')
 const { dirname } = require('path')
 const { _param_h } = require('@ctx-core/cli-args')
 const readFile = promisify(fs.readFile)
-main()
+main().then()
 async function main() {
-	const a1__path__ideaexclude = await globby('**/.ideaexclude', { gitignore: true })
-	const a1__excludeFolder = []
-	const h__param = _param_h(process.argv.slice(2), {
+	const ideaexclude_path_a1 = await globby('**/.ideaexclude', { gitignore: true })
+	const excludeFolder_a1 = []
+	const param_h = _param_h(process.argv.slice(2), {
 		indent: '-i, --indent',
 	})
-	const { indent = '' } = h__param
+	const { indent = '' } = param_h
 	await Promise.all(
-		a1__path__ideaexclude.map(async path__ideaexclude => {
-			const dir = dirname(path__ideaexclude)
-			const txt = (await readFile(path__ideaexclude)).toString().trim()
-			const a1__entry = txt.split('\n').filter(v => !!v)
-			a1__entry.forEach(entry => {
-				a1__excludeFolder.push(
+		ideaexclude_path_a1.map(async ideaexclude_path => {
+			const dir = dirname(ideaexclude_path)
+			const buffer = await readFile(ideaexclude_path)
+			const txt = buffer.toString().trim()
+			const entry_a1 = txt.split('\n').filter(v => !!v)
+			entry_a1.forEach(entry => {
+				excludeFolder_a1.push(
 					dir === '.'
 					? `${indent}<excludeFolder url="file://$MODULE_DIR$/${entry}" />`
 					: `${indent}<excludeFolder url="file://$MODULE_DIR$/${dir}/${entry}" />`
@@ -28,5 +29,5 @@ async function main() {
 			})
 		})
 	)
-	console.info(a1__excludeFolder.sort().join('\n'))
+	console.info(excludeFolder_a1.sort().join('\n'))
 }
